75659f85acf1ce64022afef8f08dae90c932cff9
NoelleDL <noelle.legrain@gmail.com>
2018-04-24 14:12:07 +0100

    Added withdraw test and method

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,18 @@
 class Account
-attr_reader :balance
+attr_reader :balance, :date, :transactions
 
   def initialize
-    @balance = []
+    @balance = 0
+    @transactions = []
   end
 
-  def deposit(money)
-    @balance << money
-  end 
+  def deposit(money, date = Time.now.strftime("%d/%m/%Y"))
+    @balance += money
+    @transactions << {date: date, credit: money, debit: nil, balance: @balance }
+  end
+
+  def withdraw(money, date = Time.now.strftime("%d/%m/%Y"))
+    @balance -= money
+    @transactions << {date: date, credit: nil, debit: money, balance: @balance }
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,12 +3,24 @@ require 'account.rb'
 describe Account do
 
   let(:subject) { described_class.new }
-  let(:fake_deposit) { double :fake_deposit }
 
   describe '#deposit' do
     it 'allows user to make deposits in account' do
-      subject.deposit(fake_deposit)
-      expect(subject.balance.length).to eq(1)
+      subject.deposit(10)
+      expect(subject.balance).to eq(10)
+    end
+
+    it 'stores a record of the transaction' do
+      subject.deposit(10)
+      expect(subject.transactions.length).to eq(1)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'allows user to withdraw funds from account' do
+      subject.deposit(10)
+      subject.withdraw(3)
+      expect(subject.balance).to eq(7)
     end
   end
 end
