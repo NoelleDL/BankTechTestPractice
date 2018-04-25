fd8baff4d1504211167143aa9ddb2b8aebdd62b3
NoelleDL <noelle.legrain@gmail.com>
2018-04-25 13:09:59 +0100

    Added print statement and test

--- lib/statement.rb
+++ lib/statement.rb
@@ -3,13 +3,19 @@ require_relative 'account.rb'
 class Statement
 
   def header
-    "date  ||  credit  ||  debit  ||  balance"
+    puts "date  ||  credit  ||  debit  ||  balance"
   end
 
   def transaction_history(account)
     account_transactions = account.transactions
     account_transactions.each do |x|
-      print x.values.join(" || ")
+      puts x.values.join("  ||  ")
     end
   end
+
+  def print_statement(account)
+    header
+    transaction_history(account)
+  end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -7,7 +7,7 @@ describe Statement do
 
   describe '#header' do
     it 'displays transaction header' do
-      expect(subject.header).to eq("date  ||  credit  ||  debit  ||  balance")
+      expect { subject.header }.to output("date  ||  credit  ||  debit  ||  balance\n").to_stdout
     end
   end
 
@@ -15,7 +15,15 @@ describe Statement do
     it 'displays the transaction history' do
       account = Account.new
       account.deposit(20)
-      expect { subject.transaction_history(account) }.to output("25/04/2018 || 20 ||  || 20").to_stdout
+      expect { subject.transaction_history(account) }.to output("25/04/2018  ||  20  ||    ||  20\n").to_stdout
+    end
+  end
+
+  describe '#print_statement' do
+    it 'displays the header and transaction history' do
+      account = Account.new
+      account.deposit(20)
+      expect { subject.print_statement(account) }.to output("date  ||  credit  ||  debit  ||  balance\n25/04/2018  ||  20  ||    ||  20\n").to_stdout
     end
   end
 end
